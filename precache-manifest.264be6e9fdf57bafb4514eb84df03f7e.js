b690d72c6675510a44f009bb03d3bb49da9caa8d
Ebru Gulec <ebru@ebrugulec.com>
2018-11-27 15:08:57 +0300

    Updates

---
+++ precache-manifest.264be6e9fdf57bafb4514eb84df03f7e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ea85d1fb4af7437e687b",
+    "url": "/github-user-timeline/static/js/runtime~main.ea85d1fb.js"
+  },
+  {
+    "revision": "2a5f1e63813fbabcec12",
+    "url": "/github-user-timeline/static/js/main.2a5f1e63.chunk.js"
+  },
+  {
+    "revision": "99ece30866d0e315542c",
+    "url": "/github-user-timeline/static/js/1.99ece308.chunk.js"
+  },
+  {
+    "revision": "2a5f1e63813fbabcec12",
+    "url": "/github-user-timeline/static/css/main.639ef3a7.chunk.css"
+  },
+  {
+    "revision": "6b4de3d4b29ba8c2d6e4d69d0fcc02ef",
+    "url": "/github-user-timeline/index.html"
+  }
+];
